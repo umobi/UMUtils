d35f993b09fb5d49acf6e1dd07c5286be83061e5
brennoumobi <brenno@umobi.com.br>
2020-04-10 15:20:47 -0300

    Added internal dependency to UMUtils

--- Package.swift
+++ Package.swift
@@ -35,7 +35,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UMUtils",
-            dependencies: ["UMUtils/Core", "UMUtils/MaterialUtils"]
+            dependencies: [
+                "UMUtils/Core", "UMUtils/MaterialUtils", "UMUtils/View",
+                "UMUtils/Rx", "UMUtils/RxAIFlatSwitch", "UMUtils/RxActivity",
+                "UMUtils/ViewModel", "UMUtils/APIModel"
+            ]
         ),
 
         .target(
